5a75fd8f38f923f30981c9e0415fa89ec26540a0
Max Ivashchenko <Max@MacBook-Pro-Max.local>
2020-07-07 16:37:11 +0300

    Task #8586 add flexible url for img in header

--- src/app/companyService/companyService.component.ts
+++ src/app/companyService/companyService.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class SFServicesComponent {
   title: string = 'WEB, MOBILE, CMS, E-COMMERCE & CLOUD SOLUTIONS';
   textBanner: string = 'StableFlow is a software development company driven to deliver uncompromising quality, efficiency, and customer service. We provide a full range of outsourcing services to create and support custom Web and Mobile applications.';
+  // imgUrl: string = 'assets/images/mac.png'
 }
