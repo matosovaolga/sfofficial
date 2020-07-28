9336054a58fb7fe2ebb6c2fea482c30190357441
Max Ivashchenko <Max@MacBook-Pro-Max.local>
2020-07-28 19:20:59 +0300

    fix header arrays

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -22,42 +22,56 @@ export class SFHeaderComponent implements OnInit {
     currentSection: string = ''
     currentSubSection: string = ''
     services = [
-        {name: "uxdesign", desc: "UX Design and Prototypes"},
-        {name: "software", desc: "Web Appliction Development"},
-        {name: "mobile", desc: "Mobile Application Development"},
-        {name: "cms", desc: "CMS Development"},
-        {name: "qa", desc: "QA and Testing"},
-        {name: "integration", desc: "Application Integration"},
-        {name: "discovery", desc: "Discovery Phase"},
-        {name: "consulting", desc: "Software Architecture & Technology Consulting"}
-    ]
-    bussinesSolutions = [
-        {name: "portals", desc: "Web and Enterprise Portals"},
-        {name: "mobile", desc: "Enterprise Mobile Apps"},
-        {name: "ecommerce", desc: "Ecommerce"},
-        {name: "social", desc: "Social Networking"},
-        {name: "automation", desc: "Business Process Automation"},
-        {name: "hospital", desc: "Hospital Managment System"},
-
+        { name: "uxdesign", desc: "UX Design and Prototypes" },
+        { name: "software", desc: "Web Appliction Development" },
+        { name: "mobile", desc: "Mobile Application Development" },
+        { name: "cms", desc: "CMS Development" },
+        { name: "qa", desc: "QA and Testing" },
+        { name: "integration", desc: "Application Integration" },
+        { name: "discovery", desc: "Discovery Phase" },
+        { name: "consulting", desc: "Software Architecture & Technology Consulting" }
     ]
 
-    developmentStack = [
-        {name: "open-source", desc: "Open Source"},
-        {name: "mobile-platforms", desc: "Mobile platforms"},
-        {name: "javascript", desc: "JavaScript"},
-        {name: "cloud-tech", desc: "Cloud Technologies"},
+    expertises = [
+        {
+            title: "Bussines solutions", subSection: [
+                { name: "portals", desc: "Web and Enterprise Portals" },
+                { name: "mobile-enterprise", desc: "Enterprise Mobile Apps" },
+                { name: "ecommerce", desc: "Ecommerce" },
+                { name: "social", desc: "Social Networking" },
+                { name: "automation", desc: "Business Process Automation" },
+                { name: "hospital", desc: "Hospital Managment System" },
+
+            ]
+        },
+        {
+            title: "Development stack", subSection: [
+                { name: "open-source", desc: "Open Source" },
+                { name: "mobile-platforms", desc: "Mobile platforms" },
+                { name: "javascript", desc: "JavaScript" },
+                { name: "cloud-tech", desc: "Cloud Technologies" },
+            ]
+        },
+    ]
+    
+    engagementModels = [
+        { name: "outsourcing", desc: "Project-based Outsourcing" },
+        { name: "dedicate", desc: "Dedicated Team" },
+        { name: "staff-augmentation", desc: "Staff Augmentation" }
     ]
 
     companySection = [
-        {name: "about", desc: "About Us"},
-        {name: "vacancies", desc: "Vacancies"},
-        {name: "process", desc: "Process"}
+        { name: "about", desc: "About Us" },
+        { name: "vacancies", desc: "Vacancies" },
+        { name: "process", desc: "Process" }
     ]
+
+
 
     click(sect, subSection) {
 
-       this.currentSection = sect;
-       this.currentSubSection = subSection;
+        this.currentSection = sect;
+        this.currentSubSection = subSection;
 
     }
 
