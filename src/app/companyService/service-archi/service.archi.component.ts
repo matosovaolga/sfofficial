01ed3691a0a1ea87a296bba85bed47d9f230b926
Ivashchenko Maksym <maksym.entex@gmail.com>
2020-09-09 11:15:48 +0300

    add title for services pages

--- src/app/companyService/service-archi/service.archi.component.ts
+++ src/app/companyService/service-archi/service.archi.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./service.archi.component.scss']
 })
 export class SFServiceArchiComponent {
-    title: string = 'SOFTWARE ARCHITECTURE CONSULTING';
-    textBanner: string = 'Artezio offers software architecture consulting in different directions: within project implementations as a part of pre-production activities and as validation services to ensure that architectural decisions were made right.';
+    title: string = 'Software Architecture & Technology Consulting';
+    textBanner: string = 'StableFlow offers software architecture consulting in different directions: within project implementations as a part of pre-production activities and as validation services to ensure that architectural decisions were made right.';
 }
