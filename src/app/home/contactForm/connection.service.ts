3a7a8b43d57f3ae1cdc3f6dbab173b2b24a3bc8b
sepavlen <sepavlen@gmail.com>
2020-09-14 14:41:21 +0300

    change path send form and remove validation phone

--- src/app/home/contactForm/connection.service.ts
+++ src/app/home/contactForm/connection.service.ts
@@ -6,13 +6,12 @@ providedIn: 'root'
 })
 export class ConnectionService {
 // url: string = 'http://localhost:3000/send';
-url: string = 'http://sfofficial.loc/mail.php';
+url: string = '/email/mail.php';
 constructor(private http: HttpClient) { }
 
 sendMessage(messageContent: any) {
-  console.log('string => ', messageContent);
   return this.http.post(this.url,
   messageContent,
-  { headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8'), responseType: 'text' });
+  { headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8'), responseType: 'json' });
 }
 }
