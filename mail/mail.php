cf0c35a113f393d826a7b08f7f901700e3f41ff1
sepavlen <sepavlen@gmail.com>
2020-09-15 15:32:47 +0300

    create upload file API

--- mail/mail.php
+++ mail/mail.php
@@ -7,23 +7,15 @@
 
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Methods: POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With"');
-//header('Content-Type: multipart/form-data');
+header("HTTP_ACCEPT: multipart/form-data");
 
-
-$json = file_get_contents('php://input');
-
-
-
-echo "<pre>";
-var_dump($json);
-echo "<br>";
-var_export($_POST);
-echo "<br>";
-var_export($_FILES);
-echo "</pre>";
-die;
+//$json = file_get_contents('php://input');
+////
+////// Converts it into a PHP object
+//$data_decode = json_decode($json);
+////
 
 
 
@@ -40,43 +32,78 @@ $mail = new PHPMailer(true);
 try {
     if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
-        $json = file_get_contents('php://input');
+        //        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.1and1.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'contact@stableflow.com';
+        $mail->Password   = 'SFpass123.';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
 
-            // Converts it into a PHP object
+
+
+        $json = file_get_contents('php://input');
         $data_decode = json_decode($json);
 
-        $data = validate($data_decode);
+        $parse_url = parse_url($_SERVER['HTTP_REFERER']);
 
-        if (!isset($data['error'])){
-            //        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-            $mail->isSMTP();
-            $mail->Host       = 'smtp.1and1.com';
-            $mail->SMTPAuth   = true;
-            $mail->Username   = 'contact@stableflow.com';
-            $mail->Password   = 'SFpass123.';
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            $mail->Port       = 587;
-
-            //Recipients
-            $mail->setFrom($data['contactFormEmail'], $data['contactFormName']);
-            $mail->addAddress('info@stableflow.com');     // add address to
+        if (trim($parse_url['path'],  '/') === 'contacts'){
 
 
-            // Content
+            $data = validateContact($data_decode, $_FILES);
 
-            ob_start();
-            include 'views/send_mail.php';
-            $str = ob_get_clean();
+            if (!isset($data['error'])){
 
-            $mail->isHTML(true);
-            $mail->Subject = $data['contactFormSubject'];
-            $mail->Body    = $str;
+                $mail->setFrom($data['contactFormEmail'], $data['contactFormName']);
+                $mail->addAddress('info@stableflow.com');     // add address to
 
-            $mail->send();
+                foreach ($_FILES['files'] as $FILE) {
+                    move_uploaded_file($FILE['tmp_name'], 'uploads/' . $FILE['name']);
+                    $mail->addAttachment('uploads/' . $FILE['name']);
+                }
 
-            echo json_encode(['success' => "Message has be sent"]);exit;
+                // Content
+                ob_start();
+                include 'views/send_mail_contacts.php';
+                $str = ob_get_clean();
+
+                $mail->isHTML(true);
+                $mail->Subject = $data['contactFormName'];
+                $mail->Body    = $str;
+
+                $mail->send();
+
+                echo json_encode(['success' => "Message has be sent"]);exit;
+
+            }
+
+            echo json_encode($data);exit;
+
+        } else {
+
+            $data = validate($data_decode);
+
+
+            if (!isset($data['error'])){
+
+                // Content
+                ob_start();
+                include 'views/send_mail.php';
+                $str = ob_get_clean();
+
+                $mail->isHTML(true);
+                $mail->Subject = $data['contactFormSubject'];
+                $mail->Body    = $str;
+
+                $mail->send();
+
+                echo json_encode(['success' => "Message has be sent"]);exit;
+            }
+            echo json_encode($data);exit;
         }
-        echo json_encode($data);exit;
+
+
     }
 } catch (Exception $e) {
     echo json_encode(['error' => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"]);exit;
@@ -118,13 +145,58 @@ function validate($data)
     return $errors;
 }
 
+function validateContact($data, $files){
+
+    $data = getCleanData($data);
+
+    $errors = [];
+
+    if (empty($data['contactFormName'])) {
+        $errors['error']['name'] = 'Field FullName required!';
+    }
+
+    if (empty($data['contactFormEmail'])) {
+        $errors['error']['email'] = 'Field E-mail required!';
+    } elseif (!filter_var($data['contactFormEmail'], FILTER_VALIDATE_EMAIL)) {
+        $errors['error']['email'] = 'E-mail incorrect!';
+    }
+
+    if (empty($data['contactFormMessage'])) {
+        $errors['error']['project'] = 'Field Project required!';
+    }
+
+    if (!empty($data['contactFormPhone']) && !preg_match("/^[0-9\-\(\)\/\+\s]*$/", $data['contactFormPhone'])) {
+        $errors['error']['phone'] = 'Phone incorrect!';
+    }
+
+
+    if (empty($files)){
+        $errors['error']['file'] = 'Files not upload!';
+    } else {
+        foreach ($files as $file){
+            if (!empty($file['size']) && $file['size'] > 50000){
+                $errors['error']['file'] = 'File ' . $file['name'] . ' size is too large!';
+            }
+        }
+    }
+
+
+    if (!$errors){
+        return $data;
+    }
+
+    return $errors;
+}
+
 function getCleanData ($data)
 {
     $arr = [];
 
     if ($data){
         foreach ($data as $key => $value) {
-            $arr[$key] = trim(strip_tags($value));
+            if (is_string($value)){
+                $arr[$key] = trim(strip_tags($value));
+            }
         }
     }
     return $arr;
