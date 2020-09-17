41c4b18228cb42c9102ab9804510c1bf89b4b09a
Ivashchenko Maksym <maksym.entex@gmail.com>
2020-09-17 12:39:37 +0300

    fix contact form

--- src/app/contactPage/requestForm/requestForm.component.ts
+++ src/app/contactPage/requestForm/requestForm.component.ts
@@ -1,6 +1,6 @@
 
 import { ConnectionService } from '../../home/contactForm/connection.service';
-import { FormGroup, FormBuilder, Validators,FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -10,40 +10,43 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./requestForm.component.scss']
 })
 export class SFRequestComponent {
+  myFiles: string[] = [];
+
+
   myForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl('',[Validators.required]),
+    email: new FormControl('', [Validators.required]),
     phone: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    company: new FormControl('', [  Validators.minLength(7)]),
-    message: new FormControl('', [ Validators.minLength(40)]),
-     
-    file: this.formBuilder.array([
-      this.formBuilder.control('', [Validators.required])
-    ]),
+    company: new FormControl('', [Validators.minLength(7)]),
+    message: new FormControl('', [Validators.minLength(40)]),
+    file: new FormControl(''),
     fileSource: new FormControl('', [Validators.required])
   });
 
-  constructor(private connectionService: ConnectionService,private http: HttpClient, private formBuilder: FormBuilder) {
+  constructor(private connectionService: ConnectionService, private http: HttpClient) {
 
   }
-  get f(){
+  get f() {
     return this.myForm.controls;
   }
-     
+
   onFileChange(event) {
-  
+
     if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.myForm.patchValue({
-        fileSource: file,
-        file: event.target.files
-      });
+      for (var i = 0; i < event.target.files.length; i++) {
+        this.myFiles.push(event.target.files[i]);
+      }
+
     }
   }
-     
-  submit(){
+
+  submit() {
     const formData = new FormData();
-    formData.append('files', this.myForm.get('file').value);
+    // formData.append('file', this.myForm.get('fileSource').value);
+    for (var i = 0; i < this.myFiles.length; i++) { 
+      formData.append("file[i]", this.myFiles[i]);
+    }
+
     formData.append('name', this.myForm.get('name').value);
     formData.append('email', this.myForm.get('email').value);
     formData.append('phone', this.myForm.get('phone').value);
