4309fc6b264b4ac0b70a0ff56d2b241d60d92df6
Ivashchenko Maksym <maksym.entex@gmail.com>
2020-09-18 11:36:11 +0300

    fix contact form

--- src/app/company/vacancies/applyForm/applyForm.component.ts
+++ src/app/company/vacancies/applyForm/applyForm.component.ts
@@ -1,9 +1,12 @@
 
 import { ConnectionService } from '../../../home/contactForm/connection.service';
-import { FormGroup, FormBuilder, Validators,FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl, FormGroupDirective } from '@angular/forms';
 import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 import { FileSelectDirective, FileDropDirective, FileUploader } from 'ng2-file-upload';
 import { HttpClient } from '@angular/common/http';
+interface File {
+  size: number,
+}
 
 @Component({
   selector: 'sf-applyForm',
@@ -12,31 +15,46 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SFApplyFormComponent {
   isFileUpload: boolean = false;
-  file:{}= null;
+  fileSizeError: boolean = false;
+
+  errorText: string = '';
+  sendSuccess: boolean = false;
+
+  file: File = null;
   myForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl('',[Validators.required]),
+    email: new FormControl('', [Validators.required]),
     phone: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    company: new FormControl('', [  Validators.minLength(7)]),
-    message: new FormControl('', [ Validators.minLength(40)]),
+    company: new FormControl('', [Validators.minLength(7)]),
+    message: new FormControl('', [Validators.minLength(40)]),
     file: new FormControl(''),
     fileSource: new FormControl('', [Validators.required])
   });
   constructor(private connectionService: ConnectionService, private http: HttpClient) {
 
   }
-  get f(){
+  get f() {
     return this.myForm.controls;
   }
-     
+
   onFileChange(event) {
-  
+
     if (event.target.files.length > 0) {
-       this.file = event.target.files[0];
-      this.myForm.patchValue({
-        fileSource: this.file
-      });
-      this.isFileUpload = true
+      this.file = event.target.files[0];
+      if (this.file.size < 5 * 1000 * 1000) {
+        this.myForm.patchValue({
+          fileSource: this.file
+        });
+        this.isFileUpload = true
+      }
+      else {
+        this.fileSizeError = true;
+        setTimeout(() => {
+          this.fileSizeError = false;
+
+        }, 5000);
+      }
+
     }
   }
   deleteFile() {
@@ -46,9 +64,9 @@ export class SFApplyFormComponent {
     this.file = null;
     this.isFileUpload = false;
   }
-  submit(){
+  submit(form: any, formDirective: FormGroupDirective) {
     const formData = new FormData();
-
+    console.log(form)
     formData.append('file', this.myForm.get('fileSource').value);
     formData.append('name', this.myForm.get('name').value);
     formData.append('email', this.myForm.get('email').value);
@@ -57,8 +75,27 @@ export class SFApplyFormComponent {
 
     this.http.post(this.connectionService.url, formData)
       .subscribe(res => {
-        console.log(res);
-        alert('Uploaded Successfully.');
+        this.sendSuccess = true;
+        setTimeout(() => {
+          this.sendSuccess = false;
+        }, 5000);
+        formDirective.resetForm();
+        this.deleteFile();
+
+      }, (error: any) => {
+        // console.log(error)
+        setTimeout(() => {
+          setTimeout(() => {
+            this.errorText = '';
+            formDirective.resetForm();
+            this.deleteFile();
+
+
+          }, 5000);
+
+          this.errorText = error.status + " " + error.statusText;
+        }, 5000);
+
       })
   }
 
