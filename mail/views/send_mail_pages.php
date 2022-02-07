de91c894837f99193a79b96121ebcd4b572d599a
Vasilenko Artem <vasilenko.artem.val@gmail.com>
2022-02-07 12:02:21 +0200

    Display bug fixed: owl-carousel arrows, submit button on MAIN page. Added file format filter for sending to main mail. Added a new line for the warning message in the form on the MAIN page. Tried to fix a mail message error

--- mail/views/send_mail_pages.php
+++ mail/views/send_mail_pages.php
@@ -2,11 +2,11 @@
 
 <p><b>Name: </b> <?php echo $data['name'] ?></p>
 <p><b>Email: </b> <?php echo $data['email']; ?></p>
-<?php if (isset($data['company'])): ?>
-    <p><b>Phone: </b> <?php echo $data['company']; ?></p>
-<?php endif ?>
 <?php if (isset($data['phone'])): ?>
-    <p><b>Subject: </b> <?php echo $data['phone']; ?></p>
+    <p><b>Phone: </b> <?php echo $data['phone']; ?></p>
+<?php endif ?>
+<?php if (isset($data['company'])): ?>
+    <p><b>Subject: </b> <?php echo $data['company']; ?></p>
 <?php endif ?>
 <?php if (isset($data['message'])): ?>
     <p><b>Message: </b> <br> <?php echo $data['message']; ?></p>
